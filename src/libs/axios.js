4e639c29a1a89ba5221d7e19d8a3997d13edcacf
donaldding <donaldding@163.com>
2019-04-04 00:46:02 +0800

    first commit

---
+++ src/libs/axios.js
@@ -0,0 +1,104 @@
+import axios from "axios";
+import store from "@/store";
+import { getToken } from "@/libs/util";
+import { Message } from "iview";
+
+// import { Spin } from 'iview'
+const addErrorLog = errorInfo => {
+  const {
+    statusText,
+    status,
+    request: { responseURL }
+  } = errorInfo;
+  let info = {
+    type: "ajax",
+    code: status,
+    mes: statusText,
+    url: responseURL
+  };
+  if (!responseURL.includes("save_error_logger"))
+    store.dispatch("addErrorLog", info);
+};
+
+class HttpRequest {
+  constructor(baseUrl = baseURL) {
+    this.baseUrl = baseUrl;
+    this.queue = {};
+  }
+  getInsideConfig() {
+    const config = {
+      baseURL: this.baseUrl,
+      headers: {
+        //
+      }
+    };
+    return config;
+  }
+  destroy(url) {
+    delete this.queue[url];
+    if (!Object.keys(this.queue).length) {
+      // Spin.hide()
+    }
+  }
+  interceptors(instance, url) {
+    // 请求拦截
+    instance.interceptors.request.use(
+      config => {
+        // 添加全局的loading...
+        if (!Object.keys(this.queue).length) {
+          // Spin.show() // 不建议开启，因为界面不友好
+        }
+        const token = getToken();
+        console.log("api set token:" + token);
+        config.headers.Authorization = token;
+
+        this.queue[url] = true;
+        return config;
+      },
+      error => {
+        return Promise.reject(error);
+      }
+    );
+    // 响应拦截
+    instance.interceptors.response.use(
+      res => {
+        console.log("%c 接口请求", "color: #4CAF50; font-weight: bold", res);
+        this.destroy(url);
+        const { data, status } = res;
+        if (res.data && (res.data.code > 400 || res.data.meta.code != 0)) {
+          Message.error(res.data.meta.msg);
+
+          return Promise.reject({
+            ...res.data.meta
+          });
+        }
+        return { data, status };
+      },
+      error => {
+        console.log("api resquest fail:", error);
+        this.destroy(url);
+        let errorInfo = error.response;
+        if (!errorInfo) {
+          const {
+            request: { statusText, status },
+            config
+          } = JSON.parse(JSON.stringify(error));
+          errorInfo = {
+            statusText,
+            status,
+            request: { responseURL: config.url }
+          };
+        }
+        addErrorLog(errorInfo);
+        return Promise.reject(error);
+      }
+    );
+  }
+  request(options) {
+    const instance = axios.create();
+    options = Object.assign(this.getInsideConfig(), options);
+    this.interceptors(instance, options.url);
+    return instance(options);
+  }
+}
+export default HttpRequest;
