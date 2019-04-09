1de4c2cf40d1b9d5cb47e555f907bcf3061eae96
donaldding <donaldding@163.com>
2019-04-09 21:45:08 +0800

    change navigation menu

--- src/components/tree-select/tree-select.vue
+++ src/components/tree-select/tree-select.vue
@@ -11,10 +11,10 @@
 </template>
 
 <script>
-import Emitter from "iview/src/mixins/emitter";
-import TreeSelectTreeItem from "./tree-select-tree.vue";
+import Emitter from 'iview/src/mixins/emitter'
+import TreeSelectTreeItem from './tree-select-tree.vue'
 export default {
-  name: "TreeSelect",
+  name: 'TreeSelect',
   mixins: [Emitter],
   components: {
     TreeSelectTreeItem
@@ -30,31 +30,31 @@ export default {
     },
     loadData: Function
   },
-  data() {
+  data () {
     return {
       isChangedByTree: true,
       isInit: true
-    };
+    }
   },
-  provide() {
+  provide () {
     return {
       parent: this
-    };
+    }
   },
   methods: {
-    handleChange(selected) {
-      if (!this.isChangedByTree) this.$emit("input", selected);
-      this.isChangedByTree = false;
+    handleChange (selected) {
+      if (!this.isChangedByTree) this.$emit('input', selected)
+      this.isChangedByTree = false
     },
-    handleTreeCheck(selectedArray) {
-      this.isChangedByTree = true;
-      this.$emit("input", selectedArray.map(item => item.id));
+    handleTreeCheck (selectedArray) {
+      this.isChangedByTree = true
+      this.$emit('input', selectedArray.map(item => item.id))
     },
-    handleClear() {
-      this.$refs.select.reset();
+    handleClear () {
+      this.$refs.select.reset()
     }
   }
-};
+}
 </script>
 
 <style lang="less">
