0b928c2010126ed530f9e9d226ae1ec5512bdcd5
hjiangwen <hjiangwen1@163.com>
2019-04-29 00:52:06 +0800

    fix server ip

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 now=$(date +"%Y%m%d%H%M%S")
 
 echo -e "***\033[32m 创建新文件夹 /var/www/html/mail_system/$now \033[0m***"
-ssh root@113.88.96.174 "mkdir -p /var/www/html/mail_system/$now"
+ssh root@47.88.12.142 "mkdir -p /var/www/html/mail_system/$now"
 
 echo -e '***\033[32m 上传文件 \033[0m***'
-scp -r dist/. root@113.88.96.174:/var/www/html/mail_system/$now
+scp -r dist/. root@47.88.12.142:/var/www/html/mail_system/$now
 
 echo -e "***\033[32m 指向新文件夹 /var/www/html/mail_system/$now \033[0m***"
-ssh root@113.88.96.174 "ln -sfn /var/www/html/mail_system/$now /var/www/html/mail_system/current"
+ssh root@47.88.12.142 "ln -sfn /var/www/html/mail_system/$now /var/www/html/mail_system/current"
