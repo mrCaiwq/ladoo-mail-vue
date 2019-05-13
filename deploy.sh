641010bd05f42bbec57cc253d2ffc0283486209f
hjiangwen <hjiangwen1@163.com>
2019-05-13 22:48:46 +0800

    change ip in deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 now=$(date +"%Y%m%d%H%M%S")
 
-echo -e "***\033[32m 创建新文件夹 /var/www/html/mail_system/$now \033[0m***"
-ssh root@47.88.12.142 "mkdir -p /var/www/html/mail_system/$now"
+echo -e "***\033[32m 创建新文件夹 /var/www/html/mailing_frontend/$now \033[0m***"
+ssh root@47.112.137.116 "mkdir -p /var/www/html/mailing_frontend/$now"
 
 echo -e '***\033[32m 上传文件 \033[0m***'
-scp -r dist/. root@47.88.12.142:/var/www/html/mail_system/$now
+scp -r dist/* root@47.112.137.116:/var/www/html/mailing_frontend/$now
 
-echo -e "***\033[32m 指向新文件夹 /var/www/html/mail_system/$now \033[0m***"
-ssh root@47.88.12.142 "ln -sfn /var/www/html/mail_system/$now /var/www/html/mail_system/current"
+echo -e "***\033[32m 指向新文件夹 /var/www/html/mailing_frontend/$now \033[0m***"
+ssh root@47.112.137.116 "ln -sfn /var/www/html/mailing_frontend/$now /var/www/html/mailing_frontend/current"
